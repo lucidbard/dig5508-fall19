2e2b0ae808f3ff016e042da3d60b14b2d8bf484d
John Murray <lucidbard@users.noreply.github.com>
2019-10-07 14:27:51 -0400

    Additional Practice

---
+++ lab1/lab1b.py
@@ -0,0 +1,33 @@
+#%% [markdown] Here are an additional set of exercises and challenges that will
+# ensure you have a solid understanding of variables, functions and iteration.
+# A Factorial is defined as the product of an integer and all of the integers below it. 
+# If the argument is not a float, then throw a value exception.
+
+# 1
+# Calculate the factorial of any number provided as an argument.
+#%%
+
+# 2
+# Create a function accepts a single string argument, and a number. If the first
+# argument isn't a string, have it throw a ValueException. Then, print out the
+# string to the console the number of times provided in the second argument.
+# Return the number of times printed.
+#%%
+
+# 3
+# Write a function that concatenates two strings together, with a space in
+# between, and returns the result. 
+#%%
+
+# 4
+# Write a function that calculates the mean of a list of numbers and returns
+#%%
+
+# 5
+# Write a function that finds the largest number in a list of numbers and
+# returns that number.
+#%%
+
+# 6
+# Write a function that sorts a list of numbers.
+#%%
