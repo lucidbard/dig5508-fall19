f5d7aad1dd2f23caaca61184aa99d35ccf5d3519
John Murray <lucidbard@users.noreply.github.com>
2019-10-09 14:06:28 -0400

    Week 6 In-Class Notes & Exercise

---
+++ ucf_module.py
@@ -0,0 +1,16 @@
+#%%
+class MyClass:
+    """A simple example class"""
+    i = 12345
+
+    def f(self):
+        return 'hello world'
+
+#%%
+import json
+data = {
+    "president": {
+        "name": "Zaphod Beeblebrox",
+        "species": "Betelgeusian"
+    }
+}
