671f6ee0d6d66be8698e3b5d4cdc69ca21f86273
John Murray <lucidbard@users.noreply.github.com>
2019-10-23 21:04:23 -0400

    Updated class_and_json.py

--- in-class/week-8/class_and_json.py
+++ in-class/week-8/class_and_json.py
@@ -1,16 +1,27 @@
 #%%
-class MyClass:
+class MyClass1:
     """A simple example class"""
     i = 12345
-    def __init__(self, name):
+    def f(self):
+        return 'hello world'
+
+#%%
+aClass = MyClass1()
+print(aClass.f())
+bClass = MyClass2()
+print(bClass.f())
+#%%
+class MyClass2:
+    """A simple example class 2"""
+    i = 12345
+    def __init__(self,name):
         self.name = name
     def f(self):
         return 'hello world from ' + self.name
-
 #%%
-aClass = MyClass("John")
+aClass = MyClass2("John")
 print(aClass.f())
-bClass = MyClass("Lucy")
+bClass = MyClass2("Lucy")
 print(bClass.f())
 
 #%%
@@ -32,4 +43,23 @@ with open('out.json', 'r') as nfh:
     print(obj)
     print(obj["president"]["name"])
 
+#%%
+from PIL import Image
+#%%
+mode = 'RGBA'
+size = (100, 100)
+color = (100,255,100,255)
+ourimage = Image.new(mode, size, color)
+ourimage
+#%%
+for x in range(ourimage.size[0]):
+    for y in range(ourimage.size[1]):
+        value = float(x)/ourimage.size[0]
+        value = int(value*255.0)
+        ourimage.putpixel((x,y),(value,value,value,255))
+ourimage
+#%%
+ourimage.getpixel((59,50))
+#%%
+ourimage.save('allblack.png')
 #%%
